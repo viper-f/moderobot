4428331da6b8981b126d87db70cc6b053adb1ff6
kate-makarova <kate.makarova@hotmail.com>
2024-09-22 19:03:44 -0400

    init

---
+++ src/bot.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+import json
+
+class Bot:
+
+    def __init__(self):
+        options = ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-gpu')
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        prefs = {"profile.managed_default_content_settings.images": 2}
+        options.add_experimental_option("prefs", prefs)
+        options.page_load_strategy = 'eager'
+        options.add_argument("user-data-dir=./profile")
+        self.driver = webdriver.Chrome(options=options)
+        with open('data/config.json') as f:
+            data = json.load(f)
+            self.base_url = data.base_url
---
+++ src/profile_update.py
@@ -0,0 +1,25 @@
+import json
+from bot import Bot
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-d", '--data', dest="data")
+(start_options, args) = parser.parse_args()
+
+data = json.loads(start_options.data)
+
+bot = Bot()
+
+WebDriverWait(bot.driver, 5).until( EC.presence_of_element_located((By.ID, "profile8")))
+form = bot.driver.find_element(By.ID, "profile8")
+for datum in data:
+    field = form.find_element(By.ID, datum['field_name'])
+    bot.driver.execute_script("arguments[0].value = arguments[1]", field, datum['value'])
+    #form.find_element(By.ID, datum['field_name']).send_keys(datum['value'])
+form.find_element(By.NAME, "update").click()
+
+bot.driver.close()
+bot.driver.quit()
