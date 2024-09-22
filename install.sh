5b4ed29ca4714ad47707c697ddbff38fd53e4346
kate-makarova <kate.makarova@hotmail.com>
2024-09-22 19:32:27 -0400

    install

---
+++ install.sh
@@ -0,0 +1,10 @@
+apt update
+apt install sudo
+apt install git
+apt install wget -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
+sudo apt install python3 python3-venv -y
+cd moderobot
+python3 -m venv ./.venv
+.venv/bin/pip install -r requirements.txt
--- server.py
+++ server.py
@@ -5,19 +5,20 @@ import json
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
-        if self.path == 'profile-update':
+        print(self.path)
+        if self.path == '/profile-update':
             request_data = self.getPostData()
-            subprocess.Popen(["venv/bin/python", "src/profile_update.py",
+            subprocess.Popen([".venv/bin/python", "src/profile_update.py",
                               "-d", request_data['data'],
                               "symbol"], stdout=open('subprocess.log', 'a'), stderr=open('subprocess.errlog', 'a'))
             self.send_response(200)
             self.end_headers()
             self.wfile.write(b'Updating. Wait a minute')
 
-        if self.path == 'login':
+        if self.path == '/login':
             request_data = self.getPostData()
-            subprocess.Popen(["venv/bin/python", "src/login.py",
-                              "-d", request_data['data'],
+            subprocess.Popen([".venv/bin/python", "src/login.py",
+                              "-d", request_data,
                               "symbol"], stdout=open('subprocess.log', 'a'), stderr=open('subprocess.errlog', 'a'))
             self.send_response(200)
             self.end_headers()
@@ -26,7 +27,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def getPostData(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        return json.loads(post_data.decode('utf-8'))
+        return post_data.decode('utf-8')
 
 httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
 httpd.serve_forever()
