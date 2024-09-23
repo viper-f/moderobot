11b4636f955d8b4d0dfcbdf523f62f766233587e
viper-f <143018173+viper-f@users.noreply.github.com>
2024-09-23 10:31:12 -0400

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,17 @@ import json
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', '*')
+        self.send_header('Access-Control-Allow-Headers', '*')
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+        return super(CORSRequestHandler, self).end_headers()
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.end_headers()
+
     def do_POST(self):
         print(self.path)
         if self.path == '/profile-update':
